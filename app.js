533b46c4686d1f9688f10d72d9d6cab43a7d5a68
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-08-03 21:51:01 +0500

    Update app.js

--- app.js
+++ app.js
@@ -3,4 +3,12 @@ function age(){
     let d1 = document.getElementById("date").value;
     let m1 = document.getElementById("month").value;
     let y1 = document.getElementById("year").value;
+
+    let date = new Date();
+
+    let d2 = date.getDate();
+    let m2 = date.getMonth() + 1;
+    let y2 = date.getFullYear();
+
+    const month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 }
