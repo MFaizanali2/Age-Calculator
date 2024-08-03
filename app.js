a380e7e866a69894cea10f0ef7fbcc57bb473a86
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-08-03 21:55:57 +0500

    Update app.js

--- app.js
+++ app.js
@@ -11,4 +11,19 @@ function age(){
     let y2 = date.getFullYear();
 
     const month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    if(d1 >d2){
+        d2 = d2 + month[m2 - 1];
+        m2 = m2 - 1;
+    }
+    if(m1 > m2){
+        m2 = m2 - 12;
+        y2 = y2 - 1;
+    }
+    let age = y2 - y1;
+    let monthDiff = m2 - m1;
+    let dayDiff = d2 - d1;
+
+    document.getElementById("age").innerHTML = `
+    Your age is ${age} year ${monthDiff} and ${dayDiff} days`
 }
